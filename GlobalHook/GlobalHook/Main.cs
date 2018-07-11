c9dceb6e698ecb8a197d1093b5e6c9a2cbb76963
VomyraK Deng <vomyrakm@gmail.com>
2018-07-11 11:13:57 +0100

    initial commit

---
+++ GlobalHook/GlobalHook/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GlobalHook;
+using System.Windows.Forms;
+
+namespace TheHook
+{
+    public class Program
+    {
+        static void Main(string[] arg)
+        {
+            Application.Run(new myHook());
+        }
+    }
+}
