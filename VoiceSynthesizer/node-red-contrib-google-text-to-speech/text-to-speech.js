74775d2d46603e34663d3fdf28e0ae9b53553451
Ming Tso <mwt16@ray01.doc.ic.ac.uk>
2018-07-12 18:56:49 +0100

    set up constant in text-to-speech.js for the request

--- VoiceSynthesizer/node-red-contrib-google-text-to-speech/text-to-speech.js
+++ VoiceSynthesizer/node-red-contrib-google-text-to-speech/text-to-speech.js
@@ -1,9 +1,20 @@
+const textToSpeech = require('@google-cloud/text-to-speech');
+const fs = require('fs');
+
+const client = new textToSpeech.TextToSpeechClient();
+
 module.exports = function(RED) {
     function TTSNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
         node.on('input', function(msg) {
-            //input = msg.payload
+            //input from previous node is msg.payload
+            //put the input data into a JSON object
+            const request = {
+              input: {text: msg.payload},
+              voice: {languageCode: 'en-US', ssmlGender: 'FEMALE'},
+              audioConfig: {audioEncoding: 'MP3'},
+            };
             //function goes here
             //output goes to msg.payload as well
 
