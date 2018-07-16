b0c950d6fa78fe95f6e327d61209d29728d7dec7
Ming Tso <mwt16@ray01.doc.ic.ac.uk>
2018-07-16 13:35:51 +0100

    commenting

--- Prototype/public/js/device.js
+++ Prototype/public/js/device.js
@@ -3,14 +3,20 @@ function sendRequest(button) {
   var link = document.getElementById("link" + button.value).value;
   var data = document.getElementById("data" + button.value).value;
   var headers = document.getElementById("headers" + button.value).value;
+  var callback_function = document.getElementById("callback_function" + button.value).value;
 
+  // Strat a http request
   var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        console.log("success");
-      }
-    };
-    xhttp.open(http_method, link, true);
-    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhttp.send(data);
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      // If the http request is successful
+      console.log("success");
+    }
+  };
+  // Open Connection
+  xhttp.open(http_method, link, true);
+  // Set headers of the http request
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  // Send the http request with the data
+  xhttp.send(data);
 }
