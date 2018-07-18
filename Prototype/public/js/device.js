c23af625833dc34b37dc5f31ca88b1ca9eb35e6c
Ming Tso <mwt16@ray01.doc.ic.ac.uk>
2018-07-18 10:53:22 +0100

    add google api on server

--- Prototype/public/js/device.js
+++ Prototype/public/js/device.js
@@ -1,9 +1,27 @@
+var callback_map = {"decode_and_play_audio": decodeAndPlay};
+
 function sendRequest(button) {
   var http_method = document.getElementById("http_method" + button.value).value;
   var link = document.getElementById("link" + button.value).value;
   var data = document.getElementById("data" + button.value).value;
-  var headers = document.getElementById("headers" + button.value).value;
+  var headers = JSON.parse(document.getElementById("headers" + button.value).value);
   var callback_function = document.getElementById("callback_function" + button.value).value;
+  var text_input = document.getElementById("text_input" + button.value).value;
+
+  // Add the text input to data if required
+  if (text_input != '') {
+    var input = document.getElementById("textbox" + button.value).value;
+    var json = JSON.parse(data);
+    var strarr = text_input.split(".");
+    console.log(text_input.split("."));
+    if (!text_input.includes(".")) {
+      json[text_input] = input
+    } else if (strarr.length = 2) {
+      json[strarr[0]][strarr[1]] = input;
+    }
+    data = JSON.stringify(json);
+    console.log(data);
+  }
 
   // Strat a http request
   var xhttp = new XMLHttpRequest();
@@ -11,12 +29,26 @@ function sendRequest(button) {
     if (this.readyState == 4 && this.status == 200) {
       // If the http request is successful
       console.log("success");
+      // If there is a callback function,
+      // find the coresponding function in the callback_map and apply.
+      if (callback_function != '') {
+        console.log(this.responseText);
+        callback_map[callback_function](this.responseText);
+      }
     }
+    console.log(this.responseText);
   };
   // Open Connection
   xhttp.open(http_method, link, true);
   // Set headers of the http request
-  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  for(var i = 0; i < headers.length; i++) {
+    var obj = headers[i];
+    xhttp.setRequestHeader(obj.header_name, obj.header_field);
+  }
   // Send the http request with the data
   xhttp.send(data);
+}
+
+function decodeAndPlay(encodedMP3) {
+  console.log(encodedMP3);
 }
--- Prototype/server.js
+++ Prototype/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
   extended: true
@@ -64,22 +65,46 @@ app.get('/device', function(req, res) {
   });
 });
 
-app.post('/request', function(req, res) {
-  console.log(req.body.device);
-  console.log(req.body.method);
-  const listQuery = "SELECT DISTINCT device FROM outputdevice";
-  const query = {
-    // give the query a unique name
-    name: 'get-methods',
-    text: 'SELECT * FROM outputdevice WHERE device = $1',
-    values: [req.query.selected]
-  };
+app.post('/get_key', function(req, res) {
+  const query = "SELECT * FROM keys WHERE header_name = \'" + req.body.header + "\'";
   pool.query(query, (err, result) => {
-    pool.query(listQuery, (listErr, listResult) => {
-      res.render('device', {
-        methods: result.rows,
-        devices: listResult.rows
-      });
-    });
+    res.send(result.rows[0].key);
+  });
+});
+
+app.post('/tts', function(req, res) {
+  var input = req.body.input
+  const query = "SELECT * FROM keys WHERE header_name = 'X-Goog-Api-Key'";
+  pool.query(query, (err, result) => {
+    var key = result.rows[0].key;
+    var data = {
+      input :{
+        text: input
+      },
+      voice :{
+        languageCode:'en-gb',
+        name:'en-GB-Standard-A',
+        ssmlGender:'FEMALE'
+      },
+      audioConfig:{
+        audioEncoding:'MP3'
+      }
+    }
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        // If the http request is successful
+        // console.log(this.responseText);
+      } else {
+        console.log(this.responseText);
+      }
+    };
+    // Open Connection
+    xhttp.open("POST", "https://texttospeech.googleapis.com/v1beta1/text:synthesize", true);
+    // Set headers of the http request
+    xhttp.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    xhttp.setRequestHeader("X-Goog-Api-Key", key);
+    // Send the http request with the data
+    xhttp.send(JSON.stringify(data));
   });
 });
