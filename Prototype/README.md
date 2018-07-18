1b412342d8d3622b2fbb30bb661d993e1bcd3f55
VomyraK Deng <vomyrakm@gmail.com>
2018-07-18 11:54:13 +0100

    added database integration

---
+++ Prototype/README.md
@@ -0,0 +1,6 @@
+#Information about the MomgoDB database#
+
+The database is hosed on Amazon AWS, and its public DNS address is hardcoded into backend.js with log-on string. 
+To authenticate log-on, create a file called "auth.json" in the same directory with log-on detail which I will announce elseware *wink wink*
+
+**Remember to untrack the json file from git if it is tracked!**
---
+++ Prototype/backend.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const filereader = require("./auth.json");
+var options = {useNewUrlParser: true, auth: {authdb: "admin"}};
+options.user = filereader.user;
+options.pass = filereader.pass;
+mongoose.connect('mongodb://35.177.37.56:27017/uc', options)
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(error => console.error('Failed to connect',error));
+
+const outputSchema = new mongoose.Schema({
+    device: String,
+    function: [{name: String, param: [Number]}]
+});
+
+const OutputDevice = mongoose.model('outputData', outputSchema, 'outputData');
+
+async function createOutput(){
+    const device = new OutputDevice({
+        device: "test",
+        function: [{name: "test", param:[1]}]
+    }
+    );
+    const a = await device.save();
+    console.log(a);
+}
+async function getOutput(){
+    const testSchema = await OutputDevice.find();
+    console.log(testSchema);
+}
+
+getOutput();
