706da80e0f05b1e7c3e37c9c3d22963d4460622a
vomyrak <vomyrakm@gmail.com>
2018-07-19 13:07:45 +0100

    added server prototype

---
+++ C_Sharp_Server/C_Sharp_Server/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+using System.Net;
+using System.Net.Http;
+
+namespace C_Sharp_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebServer ws = new WebServer(SendResponse, "http://localhost:8080/test/");
+            ws.Run();
+            Console.WriteLine("Press any key to quit");
+            Console.ReadKey();
+            ws.Stop();
+        }
+
+        public static string SendResponse(HttpListenerRequest request)
+        {
+            return string.Format("<HTML><BODY>My web page.<br>{0}</BODY></HTML>", DateTime.Now);
+        }
+    }
+}
