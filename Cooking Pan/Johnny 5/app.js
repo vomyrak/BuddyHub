3fb9e5860ed69d550eaeb9e5092b4337de64fff6
Jaceys <jaceys.tan@gmail.com>
2018-07-23 11:57:05 +0100

    added cooking pan software with johnny five

---
+++ Cooking Pan/Johnny 5/app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var five = require('johnny-five');
+
+var board = new five.Board();
+var temperature;
+
+server.listen(3000);
+
+app.use(express.static(__dirname + '/node_modules'));
+app.get('/', function(req, res, next) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+board.on('ready', function(){
+  temperature = new five.Thermometer( {
+    controller: 'TMP36',
+    pin: 'A0'
+  });
+
+  temperature.on('data', function() {
+    board.emit('tempReading', temperature.C);
+  });
+});
+
+io.sockets.on('connection', function(socket) {
+  board.on('tempReading', function(data) {
+    console.log(data);
+    socket.emit('newData', data);
+  })
+})
