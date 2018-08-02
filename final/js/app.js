20673d0ad5b07b5210a544c70f04138409a493d3
Rachel Tan <racheltan@dyn3208-25.wlan.ic.ac.uk>
2018-08-02 13:07:11 +0100

    Added stylesheet switcher

--- final/js/app.js
+++ final/js/app.js
@@ -1,5 +1,3 @@
-
-
 // Create UCdevices object
 var UCdevices = {
     RobotArm: ["Up", "Down", "Left", "Right"],
@@ -28,7 +26,7 @@ $(document).ready(function() {
         
         $('.ui-page').append("<section id=\"tab" + index + "\" class=\"tab-content active\" />" );
         $.each(getfunc, (key, value) =>{
-        $('#tab' + index).append("<button type=\"button\" style=\"padding:30px 30px; margin: 15px auto; width:300px\" class=\"ui-btn ui-corner-all ui-shadow ui-btn-b ui-btn-icon-left ui-icon-check\">"+value+"</button>" );
+        $('#tab' + index).append("<button type=\"button\" style=\"padding:30px 30px; font-size : 1.0em; margin: 15px auto; width:300px\" class=\"ui-btn ui-corner-all ui-shadow ui-btn-b ui-btn-icon-left ui-icon-check\">"+value+"</button>" );
     
     })
         
@@ -61,6 +59,4 @@ $(document).ready(function() {
     $(target_tab_selector).addClass('active');
   });
 });
-
-
 
