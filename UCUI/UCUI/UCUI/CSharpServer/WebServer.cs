4a8ce7624c9d0f1701beac24f1841f77ff27a168
VomyraK Deng <vomyrakm@gmail.com>
2018-08-06 17:04:49 +0100

    Solution file uploaded

---
+++ UCUI/UCUI/UCUI/CSharpServer/WebServer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Text;
+
+namespace CSharpServer
+{
+    class WebServer
+    {
+        //Variables declared with "readonly" keyword is a runtime constant
+        private readonly HttpListener _listener = new HttpListener();
+        private readonly Func<HttpListenerRequest, string> _respondMethod;
+        
+        //Func<in T, out TResult> specifies a method that takes parameter of type T and returns parameter of type TResult.
+        public WebServer(string[] prefixes, Func<HttpListenerRequest, string> method)
+        {
+            if (!HttpListener.IsSupported) throw new NotSupportedException("Needs Windows XP SP2, Server 2003 or later.");
+
+            if (prefixes == null || prefixes.Length == 0) throw new ArgumentException("Prefixes");
+
+            if (method == null) throw new ArgumentException("Method");
+
+            foreach (string s in prefixes)
+                _listener.Prefixes.Add(s);
+
+            _respondMethod = method;
+            _listener.Start();
+        }
+
+        public WebServer(Func<HttpListenerRequest, string> method, params string[] prefixes)
+            : this(prefixes, method) { }
+
+        public void Run()
+        {
+            ThreadPool.QueueUserWorkItem((o) =>
+            {
+                Console.WriteLine("Webserver running...");
+                try
+                {
+                    while (_listener.IsListening)
+                    {
+                        ThreadPool.QueueUserWorkItem((c) =>
+                        {
+                            var ctx = c as HttpListenerContext;
+                            try
+                            {
+                                string rstr = _respondMethod(ctx.Request);
+                                byte[] buf = Encoding.UTF8.GetBytes(rstr);
+                                ctx.Response.ContentLength64 = buf.Length;
+                                ctx.Response.OutputStream.Write(buf, 0, buf.Length);
+                            }
+                            catch { }
+                            finally
+                            {
+                                ctx.Response.OutputStream.Close();
+                            }
+                        }, _listener.GetContext());
+                    }
+                }
+                catch { }
+            });
+        }
+
+        public void Stop()
+        {
+            _listener.Stop();
+            _listener.Close();
+        }
+    }
+}
---
+++ UCUI/UCUI/UCUI/Models/UCMethods.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace UCUI.Models
+{
+    class UCMethods
+    {
+        [DllImport("User32.dll")]
+        private static extern bool SetCursorPos(int X, int Y);
+
+        static public double GetWindowLeft(Window window)
+        {
+            if (window.WindowState == WindowState.Maximized)
+            {
+                var leftField = typeof(Window).GetField("_actualLeft", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                return (double)leftField.GetValue(window) + 7.2;
+            }
+            else
+                return window.Left;
+        }
+
+        static public double GetWindowTop(Window window)
+        {
+            if (window.WindowState == WindowState.Maximized)
+            {
+                var leftField = typeof(Window).GetField("_actualTop", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                return (double)leftField.GetValue(window) + 7.2;
+            }
+            else
+                return window.Top;
+
+        }
+
+        static public void PlayMySound()
+        {
+            using (SoundPlayer player = new SoundPlayer(AppDomain.CurrentDomain.BaseDirectory + "\\sounds\\AudioFeedback.wav"))
+            {
+                player.Play();
+            }
+
+
+        }
+
+
+        static public void SetPosition(Window win)
+        {
+            Matrix m = PresentationSource.FromVisual(System.Windows.Application.Current.MainWindow).CompositionTarget.TransformToDevice;
+            double dx = m.M11;
+            double dy = m.M22;
+            var left = Convert.ToInt32((GetWindowLeft(win) + win.ActualWidth / 2) * dx);
+            var top = Convert.ToInt32((GetWindowTop(win) + win.ActualHeight / 2) * dy);
+            SetCursorPos(left, top);
+        }
+
+    }
+}
