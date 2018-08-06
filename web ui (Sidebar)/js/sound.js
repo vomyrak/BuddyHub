edff54c6396fe39e0b1280627bcac195cff63dbf
Rachel Tan <racheltan@dyn3220-252.wlan.ic.ac.uk>
2018-08-06 17:13:59 +0100

    Edited sound function

--- web ui (Sidebar)/js/sound.js
+++ web ui (Sidebar)/js/sound.js
@@ -20,11 +20,34 @@ var sound5 = function(){
     audio = new Audio('https://www.soundjay.com/button/button-21.wav');
 }
 
+function playSound1(){
+    var audio1 = new Audio('http://www.soundjay.com/button/beep-07.wav');
+    audio1.play();
+}
+
+function playSound2(){
+    var audio1 = new Audio('https://www.soundjay.com/button/button-30.wav');
+    audio1.play();
+}
+
+function playSound3(){
+    var audio1 = new Audio('https://www.soundjay.com/button/button-14.wav');
+    audio1.play();
+}
+
+function playSound4(){
+    var audio1 = new Audio('https://www.soundjay.com/button/button-35.wav');
+    audio1.play();
+}
+
+function playSound5(){
+    var audio1 = new Audio('https://www.soundjay.com/button/button-21.wav');
+    audio1.play();
+}
 function playSound () {
     audio.play();
     
 }
-
 
 function enableMute() { 
     audio.muted = true;
