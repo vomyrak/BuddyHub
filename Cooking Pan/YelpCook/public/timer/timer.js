218d243ac7ea947e22c3a3da6e7ece288b37fb11
Max <mxm.lavrov@gmail.com>
2018-08-07 12:35:26 +0100

    KABOOM

--- Cooking Pan/YelpCook/public/timer/timer.js
+++ Cooking Pan/YelpCook/public/timer/timer.js
@@ -1,5 +1,9 @@
 var timer = new Timer();
-timer.start();
+timer.start({countdown: true, startValues: {seconds: 30}});
+$('#countdownExample .values').html(timer.getTimeValues().toString());
 timer.addEventListener('secondsUpdated', function (e) {
-    $('#basicUsage').html(timer.getTimeValues().toString());
+    $('#countdownExample .values').html(timer.getTimeValues().toString());
+});
+timer.addEventListener('targetAchieved', function (e) {
+    $('#countdownExample .values').html('KABOOM!!');
 });
