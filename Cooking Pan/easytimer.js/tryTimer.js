87e91b51ce6279af751a2958780ea23b68d81099
Jaceys <jaceys.tan@gmail.com>
2018-08-07 14:59:02 +0100

    removed easytimer.js

--- Cooking Pan/easytimer.js/tryTimer.js
+++
@@ -1,6 +0,0 @@
-// var Timer = require('./dist/easytimer.min.js')
-var timer = new Timer();
-timer.start();
-timer.addEventListener('secondsUpdated', function (e) {
-    $('#basicUsage').html(timer.getTimeValues().toString());
-});
