7719eff1a005d6f9eeea9c77c29e3a93d7c9fefd
Rachel Tan <racheltan@Rachels-MacBook-Pro-2.local>
2018-08-10 22:17:42 +0100

    shorten sound functions

--- final/public/js/sound.js
+++ final/public/js/sound.js
@@ -1,57 +1,38 @@
 var audio, audiolink;
 
-var sound1 = function() {
-  audiolink = 'http://www.soundjay.com/button/beep-07.wav';
-  audio = new Audio('http://www.soundjay.com/button/beep-07.wav');
-}
 
-var sound2 = function() {
-  audiolink = 'https://www.soundjay.com/button/button-30.wav';
-  audio = new Audio('https://www.soundjay.com/button/button-30.wav');
-}
-
-var sound3 = function() {
-  audiolink = 'https://www.soundjay.com/button/button-14.wav';
-  audio = new Audio('https://www.soundjay.com/button/button-14.wav');
-}
-
-var sound4 = function() {
-  audiolink = 'https://www.soundjay.com/button/button-35.wav';
-  audio = new Audio('https://www.soundjay.com/button/button-35.wav');
-}
-
-var sound5 = function() {
-  audiolink = 'https://www.soundjay.com/button/button-21.wav';
-  audio = new Audio('https://www.soundjay.com/button/button-21.wav');
-}
-
-function playSound1() {
-  var audio1 = new Audio('http://www.soundjay.com/button/beep-07.wav');
-  audio1.play();
-}
-
-function playSound2() {
-  var audio1 = new Audio('https://www.soundjay.com/button/button-30.wav');
-  audio1.play();
-}
-
-function playSound3() {
-  var audio1 = new Audio('https://www.soundjay.com/button/button-14.wav');
-  audio1.play();
-}
-
-function playSound4() {
-  var audio1 = new Audio('https://www.soundjay.com/button/button-35.wav');
-  audio1.play();
-}
-
-function playSound5() {
-  var audio1 = new Audio('https://www.soundjay.com/button/button-21.wav');
-  audio1.play();
-}
-
-$("button, a, select").click(function() {
+$("button, a, select").not("#sound1, #sound2, #sound3, #sound4, #sound5").click(function() {
   audio.play();
+});
+
+$("#sound1").click(function(){
+    audiolink = 'http://www.soundjay.com/button/beep-07.wav';
+    audio = new Audio('http://www.soundjay.com/button/beep-07.wav');
+    audio.play();
+});
+
+$("#sound2").click(function(){
+    audiolink = 'https://www.soundjay.com/button/button-30.wav';
+    audio = new Audio('https://www.soundjay.com/button/button-30.wav');
+    audio.play();
+});
+
+$("#sound3").click(function(){
+    audiolink = 'https://www.soundjay.com/button/button-14.wav';
+    audio = new Audio('https://www.soundjay.com/button/button-14.wav');
+    audio.play();
+});
+
+$("#sound4").click(function(){
+    audiolink = 'https://www.soundjay.com/button/button-35.wav';
+    audio = new Audio('https://www.soundjay.com/button/button-35.wav');
+    audio.play();
+});
+
+$("#sound5").click(function(){
+    audiolink = 'https://www.soundjay.com/button/button-21.wav';
+    audio = new Audio('https://www.soundjay.com/button/button-21.wav');
+    audio.play();
 });
 
 
