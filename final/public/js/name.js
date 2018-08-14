c20db0bdb4841f8c64afc871e2f4938534947fbf
Rachel Tan <racheltan@Rachels-MacBook-Pro-2.local>
2018-08-14 21:35:29 +0100

    insert name

---
+++ final/public/js/name.js
@@ -0,0 +1,7 @@
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		var name = $(this).val();
+		$(this).val("");
+		$("h2").html("<span>Hi</span> " + name + "!")
+	}
+});
