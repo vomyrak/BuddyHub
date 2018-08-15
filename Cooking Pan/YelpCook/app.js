558c802d3c735659d8439e7955be1c72c50109a1
maxlvrv <mxm.lavrov@gmail.com>
2018-08-15 14:04:52 +0000

    muteunmute

--- Cooking Pan/YelpCook/app.js
+++ Cooking Pan/YelpCook/app.js
@@ -105,6 +105,6 @@ app.post("/recipes", function(req,res){
     });
 });
 
-server.listen(3000, process.env.IP, function(){
+server.listen(process.env.PORT, process.env.IP, function(){
     console.log("YelpCook server started");
 });
---
+++ Cooking Pan/YelpCook/public/js/sound.js
@@ -0,0 +1,17 @@
+function enableMute() { 
+    var tempAudioGen = "tempAudio";
+    for (var i=0;i<10;i++){
+        tempAudioGen += i.toString();
+        tempAudioGen.muted = true;
+    }
+    
+} 
+
+function disableMute() { 
+    var tempAudioGen = "tempAudio";
+    for (var i=0;i<10;i++){
+        tempAudioGen += i.toString();
+        tempAudioGen.muted = true;
+    }
+} 
+
