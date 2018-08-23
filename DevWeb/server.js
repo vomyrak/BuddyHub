b033d9c4f77484947a713f1a83bb30c5ca952a0d
Ming Tso <mwt16@ray01.doc.ic.ac.uk>
2018-08-23 23:37:42 +0100

    add homepage for developer web

---
+++ DevWeb/server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const server = require('http').createServer(app);
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const mongoose = require("mongoose");
+const filereader = require("./auth.json");
+const filereader2 = require("./keys.json");
+const filereader3 = require("./email.json");
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+  extended: true
+}));
+
+// Config and connect to mongo database
+var options = {
+  useNewUrlParser: true,
+  auth: {
+    authdb: "admin"
+  }
+};
+options.user = filereader.user;
+options.pass = filereader.pass;
+var connectString = "mongodb://" + filereader.dns + ":27017/uc";
+mongoose.connect(connectString, options)
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(error => console.error('Failed to connect', error));
+
+
+
+// Email Configuration
+
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: filereader3.email,
+    pass: filereader3.password
+  }
+});
+
+// A dictionary of online users
+const users = {};
+
+const port = 8000;
+server.listen(port);
+
+// Specify to serve files from the public directory
+app.use(express.static(__dirname + '/public'));
+
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// HTML directory
+const html_dir = __dirname + '/public/html/';
+
+app.get('/', function(req, res) {
+  res.sendFile(html_dir + 'index.html');
+});
