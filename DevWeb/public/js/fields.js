a83e5f0f03036dc1e848d807784ec1424372b84e
Ming Tso <mwt16@ray01.doc.ic.ac.uk>
2018-08-27 02:33:27 +0100

    upload detail to database and send confirmation email for CSharp devices

--- DevWeb/public/js/fields.js
+++ DevWeb/public/js/fields.js
@@ -2,7 +2,7 @@ var num_method = 1;
 var num_function = 1;
 
 const method_html = "<div>Method: <br>" +
-  "&#8195;Method Name: <input type=\"text\" minlength=\"2\" class=\"form-control\" name=\"method\" placeholder=\"Name of the method(e.g. turn on, etc.)\" required/>" +
+  "&#8195;Method Name: <input type=\"text\" minlength=\"2\" class=\"form-control\" name=\"method\" placeholder=\"Name of the method(e.g. turn on, etc.)\" required title=\"Minimum 2 character\"/>" +
   "&#8195;HTTP Method: <input type=\"text\" class=\"form-control\" name=\"httpmethod\" placeholder=\"HTTP method (GET, POST, PUT, DELETE)\" required/>" +
   "&#8195;Link: <input type=\"text\" class=\"form-control\" name=\"link\" placeholder=\"Link of the call\" required/>" +
   "&#8195;Data: <input type=\"text\" class=\"form-control\" name=\"data\" placeholder=\"JSON data to be sent\" required/>" +
@@ -12,8 +12,8 @@ const method_html = "<div>Method: <br>" +
   "<input type=\"button\" onclick=\"removeMethod(this)\" style=\"float: right; margin-right: 100px;\" value=\"Delete Method\"/> <br><br></div>";
 
 const function_html = "<div>Function: <br>" +
-  "&#8195;Function Name: <input type=\"text\" class=\"form-control\" name=\"function\" placeholder=\"Name of the function in the library(e.g. turnOn, etc.)\" required/>" +
-  "&#8195;Continuous? <input type=\"checkbox\" name=\"continuous\" value=\"continuous\"><br>" +
+  "&#8195;Function Name: <input type=\"text\" minlength=\"2\" class=\"form-control\" name=\"function\" placeholder=\"Name of the function in the library(e.g. turnOn, etc.)\" required title=\"Minimum 2 character\"/>" +
+  "&#8195;Continuous? <input type=\"checkbox\" class=\"form-control\" name=\"continuous\" value=\"true\"><br>" +
   "<input type=\"button\" onclick=\"removeFunction(this)\" style=\"float: right; margin-right: 100px;\" value=\"Delete Function\" /> <br><br></div>";
 
 function addMethod(button) {
