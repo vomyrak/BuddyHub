1811d4b4ac04e21259682996f56039064f29eeb1
Joan Tso <joantsomw@yahoo.com.hk>
2018-09-08 12:14:51 +0100

    put database in env var as well

--- web/db/connection.js
+++ web/db/connection.js
@@ -9,7 +9,7 @@ var options = {
 };
 options.user = process.env.DB_USER;
 options.pass = process.env.DB_PASS;
-var connectString = "mongodb://" + process.env.DB_DNS + ":27017/uc";
+var connectString = "mongodb://" + process.env.DB_DNS + ":27017/" + process.env.DB_NAME;
 mongoose.connect(connectString, options)
   .then(() => console.log('Connected to MongoDB...'))
   .catch(error => console.error('Failed to connect', error));
